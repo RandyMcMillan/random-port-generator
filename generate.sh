0510b1f6ae3749170f10bf605c5a61d3aec2c840
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-07-04 16:35:05 -0400

    generate.sh:return port only

--- generate.sh
+++ generate.sh
@@ -5,7 +5,7 @@ do
     random_port=$(( ((RANDOM<<15)|RANDOM) % 49152 + 10000 ))
     status="$(nc -z 127.0.0.1 $random_port < /dev/null &>/dev/null; echo $?)"
     if [ "${status}" != "0" ]; then
-        echo "$random_port is free to use";
+        echo "$random_port";
         exit;
     fi
 done
